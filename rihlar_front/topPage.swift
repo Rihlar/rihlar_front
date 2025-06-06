f34e0ece3520ff070f57742fafdd2264d06c3bb3
koudaihirata <2230051@ecc.ac.jp>
2025-06-06 12:05:26 +0900

    add: mapkitを使用した地図表示

---
+++ rihlar_front/topPage.swift
@@ -0,0 +1,19 @@
+//
+//  topPage.swift
+//  rihlar_front
+//
+//  Created by Kodai Hirata on 2025/06/06.
+//
+
+import SwiftUI
+import MapKit
+
+struct topPage: View {
+    var body: some View {
+        Map()
+    }
+}
+
+#Preview {
+    topPage()
+}
