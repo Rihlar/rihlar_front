d60047626e7d42d90eabaf8471991b96e9bed6f7
haruna1256 <haru_kawagishi@icloud.com>
2025-06-10 12:31:30 +0900

    [add] ひとまず行うことをコメントで追加した

---
+++ rihlar_front/view/profile/profileView.swift
@@ -0,0 +1,26 @@
+//
+//  profileView.swift
+//  rihlar_front
+//
+//  Created by 川岸遥奈 on 2025/06/10.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        
+//        アイコン
+//        名前　変更ボタン
+//        実績　三つ
+//        記録した写真
+//        ダラーと三列で並べる
+        
+        Text("ProfileView")
+    }
+    
+}
+
+#Preview {
+    ProfileView()
+}
