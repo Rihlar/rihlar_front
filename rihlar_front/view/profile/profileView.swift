daca94811c4437ef3b6985a353303c02e8bf2f3c
haruna1256 <haru_kawagishi@icloud.com>
2025-06-16 14:01:08 +0900

    [add] 必要情報の配置

--- rihlar_front/view/profile/profileView.swift
+++ rihlar_front/view/profile/profileView.swift
@@ -8,18 +8,71 @@
 import SwiftUI
 
 struct ProfileView: View {
+    //    仮データ
+    let images = ["tennpure1", "tennpure2", "tennpure3", "tennpure1", "tennpure2", "tennpure3"]
     var body: some View {
+        VStack(spacing: 20){
+            Spacer().frame(height: 30)
+            
+            // アイコン
+            // 仮置きとして、googleのアイコンを置いてる本番ならurl
+            Image(.googleIcon)
+                .resizable()
+                .frame(width: 100, height: 100)
+                .clipShape(Circle())
+            
+            // ユーザーネーム＋編集ボタン
+            HStack{
+                // 名前
+                Text("prayer name")
+                    .fontWeight(.bold)
+                    .font(.title3)
+                Button {
+                    print("名前変更処理")
+                } label: {
+                    Text("編集")
+                }
+                
+            }
+            // 実績バッジ
+            HStack(spacing: 20) {
+                ForEach(0..<3) { _ in
+                    Image(.king)
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .clipShape(Circle())
+                }
+            }
+            // 記録した写真
+            Text("記録した写真")
+                .font(.title3)
+                .foregroundColor(.black)
+                .padding(10)
+                .background(in: RoundedRectangle(cornerRadius: 10))
+                .backgroundStyle(.gray)
+                .padding(10)
+            
+            // ダラーと三列で並べる
+            ScrollView {
+                LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 3), spacing: 10) {
+                    ForEach(images, id: \.self) { imageName in
+                        Image(imageName)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 100, height: 100)
+                            .clipped()
+                            .cornerRadius(8)
+                    }
+                }
+                .padding(.horizontal)
+            }
+        }
+        Spacer().frame(height: 40)
         
-//        アイコン
-//        名前　変更ボタン
-//        実績　三つ
-//        記録した写真
-//        ダラーと三列で並べる
-        
-        Text("ProfileView")
     }
-    
 }
+
+
 
 #Preview {
     ProfileView()
