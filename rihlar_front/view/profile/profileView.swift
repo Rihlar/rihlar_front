399cc43b4b419e3b664c517b749779db153e7e3d
haruna1256 <haru_kawagishi@icloud.com>
2025-06-19 11:45:34 +0900

    [add] buttonのactionを追加

--- rihlar_front/view/profile/profileView.swift
+++ rihlar_front/view/profile/profileView.swift
@@ -95,27 +95,31 @@ struct ProfileView: View {
             
             // ZStack内で最前面に置くナビゲーション
             HStack(spacing: 20) {
+
                 // カメラ
-                VStack(spacing: 0) {
-                    ZStack{
-                        // 円
-                        Circle()
-                            .fill(Color.backgroundColor)
-                            .frame(width: 80, height: 80)
-                            .shadow(radius: 2)
-                        // カメラ
-                        Image("camera")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 65, height: 65)
-                            .foregroundColor(Color.textColor)
-                        
-                        
-                    }
-                    .offset(y:8)
-                    // テキスト
-                    ZStack {
-                        // 黒縁を太く（±2までの範囲）
+                Button {
+                    print("カメラが押された")
+                } label: {
+                    VStack(spacing: 0) {
+                        ZStack{
+                            // 円
+                            Circle()
+                                .fill(Color.backgroundColor)
+                                .frame(width: 80, height: 80)
+                                .shadow(radius: 2)
+                            // カメラ
+                            Image("camera")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 65, height: 65)
+                                .foregroundColor(Color.textColor)
+                            
+                            
+                        }
+                        .offset(y:8)
+                        // テキスト
+                        ZStack {
+                            // 黒縁を太く（±2までの範囲）
                             ForEach([-2, -1, 0, 1, 2], id: \.self) { x in
                                 ForEach([-2, -1, 0, 1, 2], id: \.self) { y in
                                     if x != 0 || y != 0 {
@@ -126,87 +130,91 @@ struct ProfileView: View {
                                     }
                                 }
                             }
-
-                        // メインの白文字
-                        Text("カメラ")
-                            .font(.headline)
-                            .foregroundColor(.white)
-                            .offset(y: -6)
+                            
+                            // メインの白文字
+                            Text("カメラ")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .offset(y: -6)
+                        }
                     }
                 }
-                
+
                 // ホーム
                 Button {
-                    print("ホームに戻る")
+                    print("ホームが押された")
                 } label: {
                     ZStack {
-//                        同じテキストを重ね付けする
+                        //                        同じテキストを重ね付けする
                         // 太めの黒縁（8方向＋中心からちょいズラし）
-                            ForEach([-2, -1, 0, 1, 2], id: \.self) { x in
-                                ForEach([-2, -1, 0, 1, 2], id: \.self) { y in
-                                    if x != 0 || y != 0 {
-                                        Text("ホームに戻る")
-                                            .font(.system(size: 24, weight: .bold))
-                                            .foregroundColor(.black)
-                                            .offset(x: CGFloat(x), y: CGFloat(y))
-                                    }
+                        ForEach([-2, -1, 0, 1, 2], id: \.self) { x in
+                            ForEach([-2, -1, 0, 1, 2], id: \.self) { y in
+                                if x != 0 || y != 0 {
+                                    Text("ホームに戻る")
+                                        .font(.system(size: 24, weight: .bold))
+                                        .foregroundColor(.black)
+                                        .offset(x: CGFloat(x), y: CGFloat(y))
                                 }
                             }
-
-                            // 白文字
-                            Text("ホームに戻る")
-                                .font(.system(size: 24, weight: .bold))
-                                .foregroundColor(.white)
                         }
-                        .frame(width: 180, height: 80)
-                        .background(
-                            ZStack {
-                                // 内側のボタンカラー（buttonColor）
-                                RoundedRectangle(cornerRadius: 20)
-                                    .fill(Color.buttonColor)
-                                
-                                // 左から光が当たるグラデーション
-                                RoundedRectangle(cornerRadius: 20)
-                                    .fill(
-                                        LinearGradient(
-                                            gradient: Gradient(colors: [
-                                                Color.white.opacity(0.6),
-                                                Color.white.opacity(0.0)
-                                            ]),
-                                            startPoint: .leading,
-                                            endPoint: .trailing
-                                        )
+                        
+                        // 白文字
+                        Text("ホームに戻る")
+                            .font(.system(size: 24, weight: .bold))
+                            .foregroundColor(.white)
+                    }
+                    .frame(width: 180, height: 80)
+                    .background(
+                        ZStack {
+                            // 内側のボタンカラー（buttonColor）
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(Color.buttonColor)
+                            
+                            // 左から光が当たるグラデーション
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(
+                                    LinearGradient(
+                                        gradient: Gradient(colors: [
+                                            Color.white.opacity(0.6),
+                                            Color.white.opacity(0.0)
+                                        ]),
+                                        startPoint: .leading,
+                                        endPoint: .trailing
                                     )
-                                
-                                // 縁の色（buttonFrameColor）
-                                RoundedRectangle(cornerRadius: 20)
-                                    .stroke(Color.buttonFrameColor, lineWidth: 4)
-                                    .shadow(color: Color.buttonFrameColor.opacity(0.6), radius: 4, x: 2, y: 2)
-                            }
-                        )
-                        .cornerRadius(20)
-                        .shadow(radius:5)
+                                )
+                            
+                            // 縁の色（buttonFrameColor）
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(Color.buttonFrameColor, lineWidth: 4)
+                                .shadow(color: Color.buttonFrameColor.opacity(0.6), radius: 4, x: 2, y: 2)
+                        }
+                    )
+                    .cornerRadius(20)
+                    .shadow(radius:5)
                 }
                 // メニュー
-                VStack (spacing:0){
-                    
-                    ZStack {
-                        // 円
-                        Circle()
-                            .fill(Color.backgroundColor)
-                            .frame(width: 80, height: 80)
-                            .shadow(radius: 2)
-                        // 設定
-                        Image("line.3.horizontal")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 60, height: 60)
-                            .foregroundColor(Color.textColor)
-                    }
-                    .offset(y:8)
-                    // ボタン名
-                    ZStack {
-                        // 黒縁を太く（±2までの範囲）
+                Button {
+                    print("メニューが押された")
+                } label: {
+                    VStack (spacing:0){
+                        
+                        ZStack {
+                            // 円
+                            Circle()
+                                .fill(Color.backgroundColor)
+                                .frame(width: 80, height: 80)
+                                .shadow(radius: 2)
+                            // 設定
+                            Image("line.3.horizontal")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 60, height: 60)
+                                .foregroundColor(Color.textColor)
+                        }
+                        .offset(y:8)
+                        // ボタン名
+                        ZStack {
+                            // 黒縁を太く（±2までの範囲）
                             ForEach([-2, -1, 0, 1, 2], id: \.self) { x in
                                 ForEach([-2, -1, 0, 1, 2], id: \.self) { y in
                                     if x != 0 || y != 0 {
@@ -217,12 +225,13 @@ struct ProfileView: View {
                                     }
                                 }
                             }
-
-                        // メインの白文字
-                        Text("メニュー")
-                            .font(.headline)
-                            .foregroundColor(.white)
-                            .offset(y: -6)
+                            
+                            // メインの白文字
+                            Text("メニュー")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .offset(y: -6)
+                        }
                     }
                 }
             }
