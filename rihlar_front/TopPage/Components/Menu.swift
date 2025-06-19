b715fe0cab0c47e9546e177430da8c79006db1bf
koudaihirata <2230051@ecc.ac.jp>
2025-06-19 17:52:22 +0900

    add:メニューのデザイン完成

--- rihlar_front/TopPage/Components/Menu.swift
+++ rihlar_front/TopPage/Components/Menu.swift
@@ -17,20 +17,14 @@ struct Menu: View {
                 Spacer()
                 
                 VStack(spacing: -16) {
-//                    ZStack 
-                    RoundedRectangle(cornerRadius: 0)
-                        .fill(Color("BtnColor"))
-                        .frame(width: 340, height: 90)
-                        .clipShape(
-    //                        角丸を別ファイルで作成
-                            RoundedCornerShape(corners: [.topLeft, .bottomLeft], radius: 50)
-                        )
-                        .shadow(color: Color.black.opacity(0.25), radius: 5)
-                        .zIndex(10)
+                    ProfileMenuItem{
+                        print("プロフィールをタップ")
+                    }
+                    .zIndex(10)
                         
-                        MenuList()
-                            .zIndex(1)
-                            .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
+                    MenuList()
+                        .zIndex(1)
+                        .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
                 
                 }
             }
