29751be41d6f9dde85121f5fbf99b7718964490b
haruna1256 <haru_kawagishi@icloud.com>
2025-06-23 13:57:16 +0900

    [add] 通常時だとエラーが出るため仮置きとしてコメント化した

--- rihlar_front/Route/AppRootView.swift
+++ rihlar_front/Route/AppRootView.swift
@@ -13,38 +13,39 @@ struct AppRootView: View {
     @State private var isLoggedIn = false
     
     var body: some View {
-        if isLoggedIn{
-            NavigationStack(path: $router.path) {
-                // top画面
-//                HomeView(router: router)
-//                    .navigationBarTitle(for: Route.self){
-//                        route in
-//                        switch route{
-//                        case .camera:
-//                            // カメラ画面の遷移
-//                        case .profile:
-//                            ProfileView()
-//                        case .friend:
-//                            // フレンド画面の遷移
-//                        case .gacha:
-//                            // ガチャ画面の遷移
-//                        case .items:
-//                            // アイテム画面の遷移
-//                        case .record:
-//                            // 戦績画面の遷移
-//                        case .setting:
-//                            // 設定画面の遷移
-//                        default:
-//                            EmptyView()
-//                        }
-                    }
-            }
-        } else {
-            LoginView {
-                isLoggedIn = true
-            }
-            
-        }
+        Text("0") // エラーが出るため仮おき
+//        if isLoggedIn{
+//            NavigationStack(path: $router.path) {
+//                // top画面
+////                HomeView(router: router)
+////                    .navigationBarTitle(for: Route.self){
+////                        route in
+////                        switch route{
+////                        case .camera:
+////                            // カメラ画面の遷移
+////                        case .profile:
+////                            ProfileView()
+////                        case .friend:
+////                            // フレンド画面の遷移
+////                        case .gacha:
+////                            // ガチャ画面の遷移
+////                        case .items:
+////                            // アイテム画面の遷移
+////                        case .record:
+////                            // 戦績画面の遷移
+////                        case .setting:
+////                            // 設定画面の遷移
+////                        default:
+////                            EmptyView()
+////                        }
+//                    }
+//            }
+//        } else {
+//            LoginView {
+//                isLoggedIn = true
+//            }
+//            
+//        }
     }
     
 }
