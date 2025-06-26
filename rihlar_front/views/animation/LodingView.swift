538a7e4c298a60bc6215b099f4ced1e08e93d83b
haruna1256 <haru_kawagishi@icloud.com>
2025-06-26 14:38:00 +0900

    [add] 画像を連続的に表示する

---
+++ rihlar_front/views/animation/LodingView.swift
@@ -0,0 +1,38 @@
+//
+//  LodingView.swift
+//  rihlar_front
+//
+//  Created by 川岸遥奈 on 2025/06/26.
+//
+
+import SwiftUI
+
+// ゲームが始まる前にローディングとして流れるアニメーション
+
+struct LodingView : View {
+    // 流れてるアニメーションの配列で動かすための定義
+    @State private var frameindex = 0
+    // タイマー
+    let timer = Timer.publish(every: 0.5, on: .main, in: .common).autoconnect()
+    
+    // フレーム画像の名前
+    let frames = ["loding1","loading2","loading3","loading4"]
+    
+    var body: some View{
+        VStack{
+            Image(frames[frameindex])
+                .resizable()
+                .frame(width: 200, height: 200)
+                .onReceive(timer){_ in
+                    frameindex = (frameindex + 1) % frames.count
+                }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+               .background(Color(.systemGroupedBackground))
+    }
+    
+}
+
+#Preview {
+    LodingView()
+}
