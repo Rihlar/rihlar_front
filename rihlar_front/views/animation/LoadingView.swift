6da5095928f0c5f04a4e40565b65e3738fe9b256
haruna1256 <haru_kawagishi@icloud.com>
2025-06-26 15:49:12 +0900

    [update] ローディングの時の画像をループさせる

--- rihlar_front/views/animation/LoadingView.swift
+++ rihlar_front/views/animation/LoadingView.swift
@@ -1,5 +1,5 @@
 //
-//  LodingView.swift
+//  LoadingView.swift
 //  rihlar_front
 //
 //  Created by 川岸遥奈 on 2025/06/26.
@@ -9,30 +9,34 @@ import SwiftUI
 
 // ゲームが始まる前にローディングとして流れるアニメーション
 
-struct LodingView : View {
+struct LoadingView : View {
     // 流れてるアニメーションの配列で動かすための定義
     @State private var frameindex = 0
     // タイマー
     let timer = Timer.publish(every: 0.5, on: .main, in: .common).autoconnect()
     
     // フレーム画像の名前
-    let frames = ["loding1","loading2","loading3","loading4"]
+    let frames = ["loading1","loading2","loading3","loading4"]
     
     var body: some View{
-        VStack{
+        ZStack{
+            // 背景色
+            Color.backgroundColor
+                .ignoresSafeArea()
+            // ローディング画像
             Image(frames[frameindex])
                 .resizable()
-                .frame(width: 200, height: 200)
+                .frame(width: 200, height: 150,alignment: .center)
                 .onReceive(timer){_ in
                     frameindex = (frameindex + 1) % frames.count
                 }
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-               .background(Color(.systemGroupedBackground))
+           
     }
+       
     
 }
 
 #Preview {
-    LodingView()
+    LoadingView()
 }
