08e2fb16c804e7f34f6e31cf8c1825b041cc7d77
haruna1256 <haru_kawagishi@icloud.com>
2025-06-26 16:00:26 +0900

    [add] アニメーションが最後まで行ったら画面遷移する処理の追加

--- rihlar_front/views/animation/LoadingView.swift
+++ rihlar_front/views/animation/LoadingView.swift
@@ -12,11 +12,14 @@ import SwiftUI
 struct LoadingView : View {
     // 流れてるアニメーションの配列で動かすための定義
     @State private var frameindex = 0
+    // 画面遷移フラグ
+    @State private var isNavigate = false
     // タイマー
     let timer = Timer.publish(every: 0.5, on: .main, in: .common).autoconnect()
-    
     // フレーム画像の名前
     let frames = ["loading1","loading2","loading3","loading4"]
+    
+    
     
     var body: some View{
         ZStack{
@@ -29,11 +32,22 @@ struct LoadingView : View {
                 .frame(width: 200, height: 150,alignment: .center)
                 .onReceive(timer){_ in
                     frameindex = (frameindex + 1) % frames.count
+                    
+                    // 四周目で画面遷移
+                    if frameindex == 3{
+                        isNavigate.toggle()
+                    }
                 }
+            NavigationLink(
+                destination: EmptyView(), // ←遷移先をあとで差し替え
+                isActive: $isNavigate,
+                label: { EmptyView() }
+            )
         }
-           
+        
+        
     }
-       
+    
     
 }
 
