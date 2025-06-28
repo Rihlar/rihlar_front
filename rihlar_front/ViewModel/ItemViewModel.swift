8a9db376116f9a445f568bb60547a98a778250e3
haruna1256 <haru_kawagishi@icloud.com>
2025-06-28 19:17:46 +0900

    [add] 表示するアイテムアイコンの画像

--- rihlar_front/ViewModel/ItemViewModel.swift
+++ rihlar_front/ViewModel/ItemViewModel.swift
@@ -19,13 +19,13 @@ class ItemViewModel {
     // 仮データを読み込み処理
     private func loadItems(){
         items = [
-            Item(id: 1, name: "多分守るマン", count: 3, iconName: "drop"),
-            Item(id: 2, name: "倍倍ふぁいと", count: 1, iconName: "key"),
-            Item(id: 3, name: "嬉Cー", count: 1, iconName: "map"),
-            Item(id: 4, name: "絶対押してマイルマン", count: 42, iconName: "bitcoinsign.circle"),
-            Item(id: 5, name: "一向に構わん水", count: 0, iconName: "diamond"),
-            Item(id: 6, name: "勘のいいガキー", count: 6, iconName: "diamond"),
-            Item(id: 7, name: "命を刈り取るカマ", count: 0, iconName: "diamond")
+            Item(id: 1, name: "多分守るマン", count: 3, iconName: "tabunman"),
+            Item(id: 2, name: "倍倍ふぁいと", count: 1, iconName: "fight"),
+            Item(id: 3, name: "嬉Cー", count: 1, iconName: "C-"),
+            Item(id: 4, name: "絶対押してマイルマン", count: 42, iconName: "zattaiman"),
+            Item(id: 5, name: "一向に構わん水", count: 0, iconName: "water"),
+            Item(id: 6, name: "勘のいいガキー", count: 6, iconName: "gaki-"),
+            Item(id: 7, name: "命を刈り取るカマ", count: 0, iconName: "kama")
         ]
     }
 }
