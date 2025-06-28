f51a541030ebfa1efe2f3f8107f86d99561c8ada
haruna1256 <haru_kawagishi@icloud.com>
2025-06-28 18:48:18 +0900

    [add] モデル定義

---
+++ rihlar_front/models/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  rihlar_front
+//
+//  Created by 川岸遥奈 on 2025/06/28.
+//
+
+// アイテムの要素定義
+struct Item {
+    var id: Int
+    var name: String
+    var count: Int
+    let iconName: String
+}
--- rihlar_front/views/Bag/ItemView.swift
+++ rihlar_front/views/Bag/ItemView.swift
@@ -5,3 +5,32 @@
 //  Created by 川岸遥奈 on 2025/06/27.
 //
 
+import SwiftUI
+
+struct ItemView: View {
+    var body: some View {
+        ZStack{
+            Color.backgroundColor
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Spacer()
+                Text("アイテム")
+                    
+                List{
+                    Text("aaaa")
+                }
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    ItemView()
+}
+
+// アイテムの一個の表示デザイン(アイコン、名前、数)
+func ItemList() -> some View {
+    Text("アイテム")
+}
+
