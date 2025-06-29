32275195e9d909141ecaf206172883306306f54f
haruna1256 <haru_kawagishi@icloud.com>
2025-06-29 22:09:34 +0900

    [add] 保存されたトークンからアクセストークンを取得する処理

--- rihlar_front/auth/TokenManager.swift
+++ rihlar_front/auth/TokenManager.swift
@@ -7,16 +7,18 @@
 
 import Foundation
 
-// 保存されたトークン（あれば）と、保存した時間を取り出して、3分以内ならそれを返す処理
+// Token管理クラス
 class TokenManager {
     // shared（シングルトン）を使って、アプリ内で1つだけのインスタンスにする
     static let shared = TokenManager()
-    // 保存に使うキー（UserDefaultsで使うためのラベルみたいなもの）
-    private let tokenKey = "cachedToken"
-    private let tokenTimeKey = "cachedTokenTime"
+    
+    // 保存に使うキー
+    private let tokenKey = "cachedToken"            // UserDefaultsでのトークン保存キー
+    private let tokenTimeKey = "cachedTokenTime"    // トークン保存時間のキー
     
     // 初期化をprivateにして、外から勝手にインスタンス化できないようにする
     private init(){}
+    
     
     // アクセストークンを取得する（3分以内ならキャッシュを返す）非同期処理
     func getAccessToken() async throws -> String? {
@@ -39,9 +41,44 @@ class TokenManager {
             }
             
         }
-        // トークンがない、または古い時→再取得
-        throw NSError(domain: "TokenManager", code: 404, userInfo: [NSLocalizedDescriptionKey: "キャッシュなし、または期限切れ"])
+        // ないor古い(再取得)
+        return nil
         
+    }
+    
+    // エンドポイントから新しいトークンをとってきて保持する関数
+    func fetchAndCacheAccessToken() async throws -> String {
+        // KeyChainから認証トークンを取得
+        guard let autoToken = getKeyChain(key: "autoToken") else{
+            throw NSError(domain: "TokenManager", code: 401, userInfo: [NSLocalizedDescriptionKey: "Authorizationトークンがありません"])
+        }
+        guard let url = URL(string: " https://authbase-test.kokomeow.com/auth/token")else{
+            throw NSError(domain: "TokenManager", code: 400, userInfo: [NSLocalizedDescriptionKey: "URLが不正です"])
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("autoTokeapplication/json", forHTTPHeaderField: "Content-Type")
+        request.setValue(autoToken, forHTTPHeaderField: "Authorization")
+        
+        // 非同期リクエスト
+        let (data, _) = try await URLSession.shared.data(for: request)
+        
+        // サーバーからのjson構造
+        struct TokenResponse: Codable {
+            let message: String
+            let token: String
+        }
+        // jsonをデコードしてtokenを取得
+        let response = try JSONDecoder().decode(TokenResponse.self, from: data)
+        let token = response.token
+        
+        // トークンと取得時間をUserDefaultsに保存
+        UserDefaults.standard.set(token, forKey: "tokenKey")
+        UserDefaults.standard.set(Date(), forKey: "tokenTimeKey")
+        
+        print("新しいトークンを取得しました！")
+        
+        return token
     }
     
 }
