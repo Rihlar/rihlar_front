eacf4a6c00c11e605f775140061dfac530521bff
haruna1256 <haru_kawagishi@icloud.com>
2025-06-29 13:45:17 +0900

    [add] UserProfileViewData モデルを一個にまとめた

--- rihlar_front/views/profile/UserProfileViewData.swift
+++ rihlar_front/views/profile/UserProfileViewData.swift
@@ -5,3 +5,12 @@
 //  Created by 川岸遥奈 on 2025/06/29.
 //
 
+import Foundation
+// 表示用にモデルをまとめたもの
+
+struct UserProfileViewData {
+    let user: User      // ユーザー情報
+    let record: Record  // 実績情報
+    let photos: [Photo]  // 写真情報
+}
+
