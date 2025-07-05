f616b0151f1c425c30fddebc4e50029d3448fd28
haruna1256 <haru_kawagishi@icloud.com>
2025-07-05 22:55:12 +0900

    [update] アイテム数の変更

--- rihlar_front/ViewModel/RecordsViewModel.swift
+++ rihlar_front/ViewModel/RecordsViewModel.swift
@@ -27,7 +27,7 @@ class RecordsViewModel: ObservableObject {
             Record(id: 4, title: "7日間連続で写真を撮る", imageUrl: "7daysphoto", isSelected: false),
             Record(id: 5, title: "陣取りゲーム 個人戦で5勝する", imageUrl: "soro5win", isSelected: false),
             Record(id: 6, title: "陣取りゲーム チーム戦で5勝する", imageUrl: "team5win", isSelected: true),
-            Record(id: 7, title: "アイテムを5個獲得", imageUrl: "5item", isSelected: false)
+            Record(id: 7, title: "アイテムを100個獲得", imageUrl: "100item", isSelected: false)
 
             
         ]
