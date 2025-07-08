7014052897538f68f46fa5d4facb997c0fcfcd3e
haruna1256 <haru_kawagishi@icloud.com>
2025-07-08 15:54:07 +0900

    [add] プロフィールの画面に入れるデータを追加した

--- rihlar_front/Mocks/profilwMock.swift
+++ rihlar_front/Mocks/profilwMock.swift
@@ -27,14 +27,56 @@ let mockPhotos: [Photo] = [
         id: "img1",
         url: "tennpure1",
         theme: "猫",
-        createdAt: Date()
+        createdAt: Date().addingTimeInterval(-0)
     ),
     Photo(
         id: "img2",
         url: "tennpure2",
         theme: "いちご",
-        createdAt: Date().addingTimeInterval(-86400)
-    )
+        createdAt: Date().addingTimeInterval(-1000)
+    ),
+    Photo(
+        id: "img3",
+        url: "tennpure3",
+        theme: "スイーツ",
+        createdAt: Date().addingTimeInterval(-2000)
+    ),
+    Photo(
+        id: "img4",
+        url: "tennpure4",
+        theme: "ラーメン",
+        createdAt: Date().addingTimeInterval(-3000)
+    ),
+    Photo(
+        id: "img5",
+        url: "tennpure5",
+        theme: "そら",
+        createdAt: Date().addingTimeInterval(-4000)
+    ),
+    Photo(
+        id: "img6",
+        url: "tennpure6",
+        theme: "バイク",
+        createdAt: Date().addingTimeInterval(-5000)
+    ),
+    Photo(
+        id: "img7",
+        url: "tennpure7",
+        theme: "犬",
+        createdAt: Date().addingTimeInterval(-6000)
+    ),
+    Photo(
+        id: "img8",
+        url: "tennpure8",
+        theme: "ご飯",
+        createdAt: Date().addingTimeInterval(-7000)
+    ),
+    Photo(
+        id: "img9",
+        url: "tennpure9",
+        theme: "ぬいぐるみ",
+        createdAt: Date().addingTimeInterval(-8000)
+    ),
     
     
     
