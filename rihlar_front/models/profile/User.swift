6aa12106086ffe97eb0d938a1deab74be7be0aef
haruna1256 <haru_kawagishi@icloud.com>
2025-07-09 16:01:55 +0900

    [update] ユーザー情報のモデル定義にapiからのレスポンス受け取りでの返還の仕組みを追加

--- rihlar_front/models/profile/User.swift
+++ rihlar_front/models/profile/User.swift
@@ -5,12 +5,25 @@
 //  Created by 川岸遥奈 on 2025/06/29.
 //
 import Foundation
-// ユーザー情報のモデル定義
+// APIのレスポンスに対応するユーザー情報モデル
 struct User:Codable, Identifiable{
-    let id:String   // ユーザーid
-    var name:String // ユーザー名
+    let id: String              // ユーザーid
+    let  name: String           // ユーザー名
+    let email: String?          // メールアドレス（フロント側では使い道ないかも）
+    let provCode: String?       // 認証プロバイダ（今回はgoogle）
+    let provUid: String?        // プロバイダ側のユーザーid
     
-    // ユーザーのアイコン
+    // APIのキーとswiftのプロアティのマッピング（snake_case→camelCase）
+    enum CodengKeys: String, CodingKey {
+        case id = "user_id"
+        case name
+        case email
+        case provCode = "prov_code"
+        case provUid = "prov_uid"
+    }
+    
+    
+    // ユーザーのアイコン（バックエンドでユーザーIDに応じて作成される）
     var iconUrl:URL?{
         URL(string: "https://rihlar.kokomeow.com/auth/assets/\(id).png")
     }
